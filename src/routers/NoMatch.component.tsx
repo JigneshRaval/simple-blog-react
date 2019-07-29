69be5157a158ae1b53de48270cc7e3fe4d31be10
Jignesh Raval <jigneshraval2009@gmail.com>
2019-07-29 21:02:52 +0530

    Added 404 page not found component.

---
+++ src/routers/NoMatch.component.tsx
@@ -0,0 +1,18 @@
+// 404 Error page
+
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const NoMatch = (props: any) => {
+    return (
+        <div className="notfound">
+            <div className="notfound-404">
+                <h1>4<span>0</span>4</h1>
+            </div>
+            <h2>the page you requested could not found</h2>
+            <Link to="/" className="uk-button uk-button-secondary">Take me back</Link>
+        </div>
+    );
+}
+
+export default NoMatch;
